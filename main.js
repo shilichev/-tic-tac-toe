382d2b2e66a3cf71f124dd7146b7e814bc3c1353
shilichev <shiichev@gmail.com>
2019-10-21 09:45:39 +0300

    second

--- main.js
+++ main.js
@@ -14,120 +14,12 @@ document.getElementById("game").onclick = function(event) {
     checkWinner();
   }
 };
-function checkWinner() {
-  let allblock = document.getElementsByClassName("block");
-  if (
-    allblock[0].innerHTML == "X" &&
-    allblock[1].innerHTML == "X" &&
-    allblock[2].innerHTML == "X"
-  ) {
-    alert("Победили крестики");
-  }
-  if (
-    allblock[8].innerHTML == "X" &&
-    allblock[6].innerHTML == "X" &&
-    allblock[7].innerHTML == "X"
-  ) {
-    alert("Победили крестики");
-  }
-  if (
-    allblock[3].innerHTML == "X" &&
-    allblock[4].innerHTML == "X" &&
-    allblock[5].innerHTML == "X"
-  ) {
-    alert("Победили крестики");
-  }
-  if (
-    allblock[0].innerHTML == "X" &&
-    allblock[4].innerHTML == "X" &&
-    allblock[8].innerHTML == "X"
-  ) {
-    alert("Победили крестики");
-    
-  }
-  if (
-    allblock[2].innerHTML == "X" &&
-    allblock[4].innerHTML == "X" &&
-    allblock[6].innerHTML == "X"
-  ) {
-    alert("Победили крестики");
-  }
-  if (
-    allblock[0].innerHTML == "X" &&
-    allblock[3].innerHTML == "X" &&
-    allblock[6].innerHTML == "X"
-  ) {
-    alert("Победили крестики");
-  }
-  if (
-    allblock[1].innerHTML == "X" &&
-    allblock[4].innerHTML == "X" &&
-    allblock[7].innerHTML == "X"
-  ) {
-    alert("Победили крестики");
-  }
-  if (
-    allblock[2].innerHTML == "X" &&
-    allblock[5].innerHTML == "X" &&
-    allblock[8].innerHTML == "X"
-  ) {
-    alert("Победили крестики");
-  }
-  if (
-    allblock[0].innerHTML == "0" &&
-    allblock[1].innerHTML == "0" &&
-    allblock[2].innerHTML == "0"
-  ) {
-    alert("Победили нолики");
-  }
-  if (
-    allblock[8].innerHTML == "0" &&
-    allblock[6].innerHTML == "0" &&
-    allblock[7].innerHTML == "0"
-  ) {
-    alert("Победили нолики");
-  }
-  if (
-    allblock[3].innerHTML == "0" &&
-    allblock[4].innerHTML == "0" &&
-    allblock[5].innerHTML == "0"
-  ) {
-    alert("Победили нолики");
-  }
-  if (
-    allblock[0].innerHTML == "0" &&
-    allblock[4].innerHTML == "0" &&
-    allblock[8].innerHTML == "0"
-  ) {
-    alert("Победили нолики");
-    
-  }
-  if (
-    allblock[2].innerHTML == "0" &&
-    allblock[4].innerHTML == "0" &&
-    allblock[6].innerHTML == "0"
-  ) {
-    alert("Победили нолики");
-  }
-  if (
-    allblock[0].innerHTML == "0" &&
-    allblock[3].innerHTML == "0" &&
-    allblock[6].innerHTML == "0"
-  ) {
-    alert("Победили нолики");
-  }
-  if (
-    allblock[1].innerHTML == "0" &&
-    allblock[4].innerHTML == "0" &&
-    allblock[7].innerHTML == "0"
-  ) {
-    alert("Победили нолики");
-  }
-  if (
-    allblock[2].innerHTML == "0" &&
-    allblock[5].innerHTML == "0" &&
-    allblock[8].innerHTML == "0"
-  ) {
-    alert("Победили нолики");
-  }
+var allblock = document.getElementsByClassName("block");
+
+function checkWinner () {
+    var combo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (let i = 0; i < combo.length; i++) {
+        if (allblock[combo[i][0]].innerHTML == "X" && allblock[combo[i][1]].innerHTML == "X" && allblock[combo[i][2]].innerHTML == "X") alert ('Крестики');
+        if (allblock[combo[i][0]].innerHTML == "0" && allblock[combo[i][1]].innerHTML == "0" && allblock[combo[i][2]].innerHTML == "0") alert ('Нолики');
+    }
 }
